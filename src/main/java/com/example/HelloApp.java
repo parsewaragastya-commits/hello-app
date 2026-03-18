6ac7d26ce11b174dcc1351e846176b0a13b62960
parsewaragastya-commits <parsewaragastya@gmail.com>
2026-03-18 10:43:18 +0530

    UC2: Display Hello with command-line argument

--- src/main/java/com/example/HelloApp.java
+++ src/main/java/com/example/HelloApp.java
@@ -2,6 +2,13 @@ package com.example;
 
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // Check if user provided input
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, User!");
+        }
     }
 }
